b6f3c4359c6eed0ef64aada5f8b642f23c59fa47
yunggarlic <ferrari.tim.v@gmail.com>
2021-01-07 11:49:47 -0600

    updates heroku

--- server/db/database.js
+++ server/db/database.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/boilerplate', {
-  logging: console.log,
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/boilerplate',
+  {
+    logging: console.log,
+  }
+);
 
 module.exports = db;
